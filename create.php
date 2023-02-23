8fb25015a226c8c1da5cf5dc6540c54ebcbf8021
GiadaMarzapane <giada.ortesta@gmail.com>
2023-02-23 18:02:40 +0100

    push nuova task in database con form

--- create.php
+++ create.php
@@ -1,5 +1,18 @@
 <!-- creato file php per post api -->
 <?php
-    header('Content-Type: application/json');
+    // recupero il mio file database json
+    $tasksString = file_get_contents('database.json');
 
+    // decode per renderlo un array associativo
+    $tasks = json_decode($tasksString, true);
+
+    // pusho la nuova task nell'array del database
+    $tasks[] = $_POST['task'];
+
+    $encodedTask = json_encode($tasks);
+    
+    file_put_contents('database.json', $encodedTask);
+    
+    header('Content-Type: application/json');
+    
     echo json_encode($_POST);
